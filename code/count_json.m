61cfcb1cecfe0915992b4e2075a1d3899efaa36b
mattybass <daniele.mattedi@studenti.unitn.it>
2020-10-23 19:07:58 +0200

    count parameters

---
+++ code/count_json.m
@@ -0,0 +1,44 @@
+function [T] = count_json(input_file)
+%COUNT_JSON Summary of this function goes here
+%   Detailed explanation goes here
+fileName = ['dataset\Scope Definition & Inception\data\', input_file, '.geojson']; % filename in JSON extension
+
+
+fid = fopen(fileName); 
+raw = fread(fid,inf); 
+str = char(raw'); 
+fclose(fid); 
+val = jsondecode(str);
+total=0;
+for i=1:size(val.features,1)
+    temp=fieldnames(val.features(i).properties);
+    for j=1:size(temp,1)
+        temp(j)
+        if(i==1 && j==1)
+            check(1)=temp(j);
+            check_num(1)=1;
+        else
+            check_vect=ismember(check,temp(j));
+            if(sum(check_vect)==0)
+               check(size(check,2)+1)=temp(j);
+               check_num(size(check,2))=1;
+            else
+                ind=find(check_vect);
+                check_num(ind)=check_num(ind)+1;
+            end
+        end
+    end
+    total=size(val.features,1);
+    
+outtable=check';
+for i=1:size(outtable,1)
+    outtable{i,2}=check_num(1,i);
+    outtable{i,3}=outtable{i,2}/total*100;
+end
+T = cell2table(outtable,'VariableNames',{'Name' 'Count' 'Percentual'});
+name=['dataset\Scope Definition & Inception\metadata\count_',input_file,'.csv'];
+writetable(T,name);
+end
+
+
+
