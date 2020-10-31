357269ed9634dadefcbc602d811d6015a3c691c6
Michele Tessari <michele.tessari12@gmail.com>
2020-10-31 17:50:03 +0100

    filtered dataset (missing punti di interesse and building)

--- code/data_filtering.py
+++ code/data_filtering.py
@@ -1,36 +1,64 @@
 # script to filter out useless data
 
 import json
+import csv
 from os import path
 from sys import argv
 
 IN_FOLDER = "./dataset/Scope Definition & Inception/data/"
 OUT_FOLDER = "./dataset/Informal Modeling/data/"
-ignore_existing =  argv[1] if len(argv) > 1 else False
+ignore_existing = argv[1] if len(argv) > 1 else True
 
 filterMap = json.load(open("./code/data_filtering_config.json", "r"))
 
-for i, (k, v) in enumerate(filterMap.items()):
+for mapIndex, (k, v) in enumerate(filterMap.items()):
 	if ignore_existing and path.exists(OUT_FOLDER + k):
 		continue
-	dataJson = open(IN_FOLDER + k, "r")
-	data = json.load(dataJson)
-	for it in v["iterator"].split('.'):
-		data = data[it]
 
+	IsJSON = False if k.split('.')[1] == "csv" else True
+	data = None
+	dataset = open(IN_FOLDER + k, "r")
 	filteredData = {"records": []}
 
-	for d_i, d in enumerate(data):
-		newRecord = {}
-		for prop in v["props"]:
-			fields = prop.split('.')
-			subFieldRef = d
-			for j, subfield in enumerate(fields):
-				if j < len(fields) - 1:
-					subFieldRef = subFieldRef[subfield]
-				elif subfield in subFieldRef:
-					newRecord[subfield] = subFieldRef[subfield]
-		filteredData["records"].append(newRecord)
+	print(k)
 
-	with open(OUT_FOLDER + k, 'w+') as file:
-		json.dump(filteredData, file)
+	if IsJSON:
+		data = json.load(dataset)
+		for it in v["iterator"].split('.'):
+			if len(it) > 0:
+				data = data[it]
+		for d_i, d in enumerate(data):
+			newRecord = {}
+			for prop in v["props"]:
+				fields = prop.split('.')
+				subFieldRef = d
+				for j, subfield in enumerate(fields):
+					if j < len(fields) - 1:
+						subFieldRef = subFieldRef[subfield]
+					elif subfield in subFieldRef:
+						newRecord[subfield] = subFieldRef[subfield]
+			filteredData["records"].append(newRecord)
+
+		with open(OUT_FOLDER + k, 'w+') as file:
+			json.dump(filteredData, file)
+
+	else:
+		data = csv.reader(dataset, delimiter=";")
+		filedsToSave = v["props"]
+		fields = []
+		fieldsIndex = []
+		for row_i, row in enumerate(data):
+			newRecord = {}
+			if row_i == 0:
+				fields = row
+				for i, d in enumerate(row):
+					if d in filedsToSave:
+						fieldsIndex.append(i)
+			else:
+				for i, d in enumerate(row):
+					if i in fieldsIndex and len(d) > 0:
+						newRecord[fields[i]] = d
+				filteredData["records"].append(newRecord)
+		
+		with open(OUT_FOLDER + k.split('.')[0] + ".json", 'w+') as file:
+			json.dump(filteredData, file)
