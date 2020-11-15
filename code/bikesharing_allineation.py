59d187920fb82ce8b7803fa0e08c8cadb4ba7b40
Michele Tessari <michele.tessari12@gmail.com>
2020-11-15 16:39:00 +0100

    bikesharing allineation

---
+++ code/bikesharing_allineation.py
@@ -0,0 +1,33 @@
+# script to allineate point of interests
+
+import json
+import csv
+from os import path,listdir
+from sys import argv
+
+IN_FOLDER = "./dataset/Informal Modeling/data/bikesharing_stations/"
+OUT_FOLDER = "./dataset/Formal Modeling/data/"
+ignore_existing = argv[1] if len(argv) > 1 else True
+
+bikesharing_stations = {"records":[]}
+
+
+for count, filename in enumerate(listdir(IN_FOLDER)):
+	dataset = open(IN_FOLDER + filename, "r")
+
+	print(filename)
+	cityname = filename.replace("-"," ")
+
+	data = json.load(dataset)["records"]
+
+	for d_i, d in enumerate(data):
+		if "position" in d:
+			d["GeoCoordinate"] = d.pop("position")
+		if cityname != "":
+			d["city"] = cityname
+		bikesharing_stations["records"].append(d)
+
+
+
+with open(OUT_FOLDER + "bikesharing_stations.json", 'w+') as file:
+	json.dump(bikesharing_stations, file)
