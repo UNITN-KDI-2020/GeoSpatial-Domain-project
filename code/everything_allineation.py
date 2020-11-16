4d1add89a006dab61eb1646fc25e962988e779d3
Michele Tessari <michele.tessari12@gmail.com>
2020-11-16 11:08:50 +0100

    skyresort allineation

--- code/everything_allineation.py
+++ code/everything_allineation.py
@@ -9,18 +9,23 @@ IN_FOLDER = "./dataset/Informal Modeling/data/"
 OUT_FOLDER = "./dataset/Formal Modeling/data/"
 ignore_existing = argv[1] if len(argv) > 1 else True
 
-exceptions = ["SAT_trails.json", "skiresort.json","skiResorts_currentState.json"]
+exceptions = ["SAT_trails.json","skiResorts_currentState.json"]
 
 for count, filename in enumerate(listdir(IN_FOLDER)):
-	if not ".json" in filename or filename in exceptions:
+	if not ".json" in filename or (ignore_existing and path.exists(OUT_FOLDER + filename)) or filename in exceptions:
 		continue
 
+	print(filename)
 	dataset = open(IN_FOLDER + filename, "r")
+	data = json.load(dataset)["records"]
+
 	newDataset = {"records":[]}
 
-	print(filename)
-
-	data = json.load(dataset)["records"]
+	if "skiresort.json" in filename:
+		for d_i, d in enumerate(data):
+			d["Total lenght"] = d["Total lenght"][7:] # "Total: " removing
+			d["Number of lifts"] = d["Number of lifts"][7:] # "Total: " removing
+			d["raiting"] = d["raiting"][:-15] # " stars out of 5" removing
 
 	for d_i, d in enumerate(data):
 		if "geometry" in d:
@@ -37,6 +42,12 @@ for count, filename in enumerate(listdir(IN_FOLDER)):
 			d["HouseNumber"] = d.pop("civico")
 		if "barrato" in d:
 			d["BarNumber"] = d.pop("barrato")
+		if "fumetto" in d:
+			d["address"] = d.pop("fumetto")
+		if "desvia" in d:
+			d["address"] = d.pop("desvia")
+		if "sobborgo" in d:
+			d["city"] = d.pop("sobborgo")
 		newDataset["records"].append(d)
 
 	with open(OUT_FOLDER + filename, 'w+') as file:
